7fcd3de1515ee1d596d95c8248794f6d3b6d05f1
oscar <oscar.oosterling@hva.nl>
2021-02-02 14:16:29 +0100

    Triangle mesh
    Following https://www.youtube.com/playlist?list=PL5KbKbJ6Gf9-d303Lk8TGKCW-t5JsBdtB

---
+++ Assets/Scripts/TriangleMesh.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter)),RequireComponent(typeof(MeshRenderer))]
+public class TriangleMesh : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        GenerateMeshData();
+        UpdateMesh();
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+    private void GenerateMeshData()
+    {
+        vertices = new Vector3[] { new Vector3(0,0,0), new Vector3(0, 0, 1) , new Vector3(1, 0, 0) };
+        triangles = new int[] { 0, 1, 2 };
+    }
+}
