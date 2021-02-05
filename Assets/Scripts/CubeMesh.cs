ef66454c1c0192d39f4db3676af4650b67f7831b
oscar <oscar.oosterling@hva.nl>
2021-02-05 15:20:09 +0100

    added uv

--- Assets/Scripts/CubeMesh.cs
+++ Assets/Scripts/CubeMesh.cs
@@ -9,14 +9,15 @@ public class CubeMesh : MonoBehaviour
 
     private List<Vector3> vertices;
     private List<int> triangles;
-    
+
+    private List<Vector2> uvs;
 
     [SerializeField]
     private float scale = 1f;
     private float adjustedScale;
 
     [SerializeField]
-    private Vector3 position = new Vector3( 0, 0, 0);
+    private Vector3 position = new Vector3(0, 0, 0);
 
     private void Awake()
     {
@@ -26,7 +27,7 @@ public class CubeMesh : MonoBehaviour
 
     private void Start()
     {
-        GenerateMeshData(adjustedScale,position*scale);
+        GenerateMeshData(adjustedScale, position * scale);
         UpdateMesh();
     }
     private void UpdateMesh()
@@ -34,6 +35,7 @@ public class CubeMesh : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
     }
 
@@ -41,11 +43,55 @@ public class CubeMesh : MonoBehaviour
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
+        uvs = new List<Vector2>();
 
         for (int i = 0; i < 6; i++)
         {
-            vertices.AddRange(CubeMeshData.quadVertices(i,scale,position));
+            vertices.AddRange(CubeMeshData.quadVertices(i, scale, position));
+
+
             int verticesCount = vertices.Count;
+
+            switch (i)
+            {
+                case 0:
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].y));
+                    break;
+                case 1:
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].z, vertices[verticesCount - 4].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].z, vertices[verticesCount - 3].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].z, vertices[verticesCount - 2].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].z, vertices[verticesCount - 1].y));
+                    break;
+                case 2:
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].y));
+                    break;
+                case 3:
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].z, vertices[verticesCount - 3].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].z, vertices[verticesCount - 4].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].z, vertices[verticesCount - 1].y));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].z, vertices[verticesCount - 2].y));
+                    break;
+                case 4:
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].z));
+                    break;
+                case 5:
+                    uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].z));
+                    uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].z));
+
+                    break;
+            }
 
             triangles.Add(verticesCount - 4);
             triangles.Add(verticesCount - 3);
--- Assets/Scripts/VoxelMesh.cs
+++ Assets/Scripts/VoxelMesh.cs
@@ -10,6 +10,9 @@ public class VoxelMesh : MonoBehaviour
     private List<Vector3> vertices;
     private List<int> triangles;
 
+    private List<Vector2> uvs;
+
+
 
     [SerializeField]
     private float scale = 1f;
@@ -37,6 +40,7 @@ public class VoxelMesh : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
     }
     private VoxelMeshData GenerateVoxelMeshData(int voxelSize)
@@ -70,6 +74,7 @@ public class VoxelMesh : MonoBehaviour
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
+        uvs = new List<Vector2>();
         for (int x = 0; x < voxelMeshData.Width; x++)
         {
             for (int y = 0; y < voxelMeshData.Height; y++)
@@ -94,6 +99,49 @@ public class VoxelMesh : MonoBehaviour
             {
                 vertices.AddRange(CubeMeshData.quadVertices(i, scale, position));
                 int verticesCount = vertices.Count;
+
+                switch (i)
+                {
+                    case 0:
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].y));
+                        break;
+                    case 1:
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].z, vertices[verticesCount - 4].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].z, vertices[verticesCount - 3].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].z, vertices[verticesCount - 2].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].z, vertices[verticesCount - 1].y));
+                        break;
+                    case 2:
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].y));
+                        break;
+                    case 3:
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].z, vertices[verticesCount - 3].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].z, vertices[verticesCount - 4].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].z, vertices[verticesCount - 1].y));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].z, vertices[verticesCount - 2].y));
+                        break;
+                    case 4:
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].z));
+                        break;
+                    case 5:
+                        uvs.Add(new Vector2(vertices[verticesCount - 3].x, vertices[verticesCount - 3].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 4].x, vertices[verticesCount - 4].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 1].x, vertices[verticesCount - 1].z));
+                        uvs.Add(new Vector2(vertices[verticesCount - 2].x, vertices[verticesCount - 2].z));
+                        break;
+                    default:
+                        break;
+
+                }
 
                 triangles.Add(verticesCount - 4);
                 triangles.Add(verticesCount - 3);
